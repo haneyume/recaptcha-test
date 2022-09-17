7d9433a118064da4a60d556a0f2d2d1db089e75d
haneyume <haneyume@gmail.com>
2022-09-17 15:59:39 +0800

    feat: add recaptcha api

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+import axios from 'axios';
 
 const app = express();
 
@@ -13,8 +14,23 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/api', (req, res) => {
-  res.send('Hello World!');
+app.post('/api/submit', async (req, res) => {
+  const secret = process.env.RECAPTCHA_SECRET_KEY;
+  const token = req.body.token;
+
+  const response = await axios.post(
+    `https://www.google.com/recaptcha/api/siteverify?secret=${secret}&response=${token}`,
+    {},
+    {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
+      },
+    },
+  );
+
+  console.log(response.data);
+
+  res.json({ data: response.data });
 });
 
 app.use(express.static('public'));
